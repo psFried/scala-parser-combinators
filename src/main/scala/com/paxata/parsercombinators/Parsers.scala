659d242f3810584d3bb450b3d40620c2eb582f41
Phil Fried <pfried@paxata.com>
2016-11-14 18:30:10 -0500

    basic working outline for parser combinators, one working parser jsut for a test

--- src/main/scala/com/paxata/parsercombinators/Parsers.scala
+++ src/main/scala/com/paxata/parsercombinators/Parsers.scala
@@ -1,7 +1,52 @@
 package com.paxata.parsercombinators
 
-object Parsers {
-  def foo(): String = "bar"
+trait Parsers {
+
+  type Input
+
+  def advance(input: Input, amount: Int): Input
+
+  type ParseResult[T] = Either[ParseError[Input], ParseSuccess[Input, T]]
+
+  type Parser[T] = (Input) => ParseResult[T]
+
+  def run[T](input: Input, parser: Parser[T]): T = ???
+
+  def oneOf[T](parsers: Parser[T]*): ParseResult[T] = ???
+
+  def createError[T](location: Int): ParseResult[T] = {
+    Left(ParseError(location))
+  }
+
+  def createSuccess[T](input: Input, value: T): ParseResult[T] = {
+    Right(ParseSuccess(input, value))
+  }
+}
+
+object ByteArrayParsers extends Parsers {
+  type Input = Array[Byte]
+
+  override def advance(input: Input, amount: Int): Input = {
+    input.slice(amount, input.length)
+  }
+
+  def exactMatchString(toMatch: String): Parser[String] = {
+    (bytes: Input) => {
+      val strLen = toMatch.length()
+
+      val matches = toMatch.getBytes().toStream.zip(bytes.toStream).map((lr) => lr._1 == lr._2).takeWhile(_ == true).length
+
+      if (matches == strLen) {
+        createSuccess(advance(bytes, matches), toMatch)
+      } else {
+        createError(matches + 1) //location should point to the first non-matching character
+      }
+    }
+  }
 
 }
+
+final case class ParseError[I](location: Int)
+final case class ParseSuccess[I, +T](remaining: I, value: T)
+
 
--- src/test/scala/com/paxata/parsercombinators/ParsersTest.scala
+++ src/test/scala/com/paxata/parsercombinators/ParsersTest.scala
@@ -4,13 +4,30 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.path
 
+import scala.util.Either.RightProjection
+
 @RunWith(classOf[JUnitRunner])
 class ParsersTest extends path.FunSpec {
 
-  describe("Parsers") {
-    
-    it("foo() returns foo") {
-      assert("foo" == Parsers.foo())
+  describe("ByteArrayParsers") {
+    val string = "theString"
+    val subject = ByteArrayParsers.exactMatchString(string)
+
+    describe("exactMatchString") {
+      it("succeeds if the start of the input matches the string exactly") {
+        val input = (string + "moar bytes!").getBytes()
+
+        val result = subject.apply(input).right.get
+
+        assert(string === result.value)
+        assert("moar bytes!".getBytes() === result.remaining)
+      }
+
+      it("returns an error if the input does not start with the string") {
+        val input = "theStrinAAAAAAAAA".getBytes()
+        val result = subject.apply(input).left.get
+        assert(9 === result.location)
+      }
     }
   }
 }
